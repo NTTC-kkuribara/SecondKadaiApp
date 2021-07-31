83cefef8d5fdb514e34995b8db7afb1a1bc5d8fa
NTTC-kkuribara <kattun.kuribara@ntt.com>
2021-07-31 23:42:16 +0900

    Initial Commit

---
+++ SecondKadaiApp2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SecondKadaiApp2
+//
+//  Created by ITMS NTTcom on 2021/07/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
